b2705179245a619e5353f5862d4d5209e808ccef
OrdiDoges <ordidoges@gmail.com>
2025-03-10 10:19:00 +0800

    Initial Commit

---
+++ baobao/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  baobao
+//
+//  Created by 钟庆标 on 2025/3/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ baobaoTests/baobaoTests.swift
@@ -0,0 +1,17 @@
+//
+//  baobaoTests.swift
+//  baobaoTests
+//
+//  Created by 钟庆标 on 2025/3/10.
+//
+
+import Testing
+@testable import baobao
+
+struct baobaoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
